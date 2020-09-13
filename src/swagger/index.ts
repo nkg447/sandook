1ed62083f40362defe0f31d1479909f4c6537f2c
Nikunj Gupta <nikunj.gupta@hashedin.com>
2020-09-13 20:25:24 +0530

    prod config

--- src/swagger/index.ts
+++ src/swagger/index.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { readFileSync } from 'fs';
-import { join } from 'path';
 import { set } from 'lodash';
+import { join } from 'path';
 import * as swaggerUi from 'swagger-ui-express';
 
 const router = Router();
 
 const swaggerDoc: object = JSON.parse(readFileSync(join(__dirname, 'swagger.json'), { encoding: 'utf-8' }));
 
-set(swaggerDoc, 'host', `${process.env['APP.HOST']}:${process.env['APP.PORT']}`);
+set(swaggerDoc, 'host', `${window.location.host}`);
 set(swaggerDoc, 'schemes', [process.env['APP.SCHEME']]);
 
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
